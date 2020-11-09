b4577c54370482333d36aa5c040a2f3e7474ab6a
GindaChen <32371474+GindaChen@users.noreply.github.com>
2020-11-09 09:13:52 -0600

    Formalize table entities name

--- database/table.py
+++ database/table.py
@@ -6,18 +6,27 @@ So when the
 
 class DatabaseColumn():
     def __init__(self, name: str = None, table: 'DatabaseTable' = None):
-        self.name = name or ""
+        self.cname = name or ""
         self.table: 'DatabaseTable' = table
         self.info = {}
-    pass
+
+    @property
+    def name(self) -> str:
+        """Suppose the qualified name is <tname>.<cname>"""
+        tname = self.table.tname
+        cname = self.cname
+        return f'{tname}.{cname}'
 
 
 class DatabaseTable():
     def __init__(self, name: str, database: 'Database' = None):
-        self.name = name
+        self.tname = name
         self.database: 'Database' = database
         self.info = {}
-    pass
+
+    @property
+    def name(self) -> str:
+        return self.name
 
 class Database():
     def __init__(self, name):
