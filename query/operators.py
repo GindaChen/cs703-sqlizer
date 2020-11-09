d55407778300a849c0d0bb44b2d598e9987c10e5
GindaChen <32371474+GindaChen@users.noreply.github.com>
2020-11-09 00:34:21 -0600

    Add boolean operators

--- query/operators.py
+++ query/operators.py
@@ -1,12 +1,14 @@
 from query.base import Operator
 
 eq = Operator.BinaryBoolean('=')
-neg = Operator.UnaryBoolean('!')
 lt = Operator.BinaryBoolean('<')
 gt = Operator.BinaryBoolean('>')
 le = Operator.BinaryBoolean('<=')
 ge = Operator.BinaryBoolean('>=')
 
+neg = Operator.UnaryBoolean('!')
+and_ = Operator.BinaryBoolean('AND')
+or_ = Operator.BinaryBoolean('OR')
 
 
 # Note: != is just !( x = y ). Think about if we want to keep it.
