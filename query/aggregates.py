479b75e96cf5e167d0e01418ab7a00e03d3a868f
GindaChen <32371474+GindaChen@users.noreply.github.com>
2020-11-09 00:52:14 -0600

    Move operators together

--- query/aggregates.py
+++ query/aggregates.py
@@ -1,7 +1,2 @@
 from query.base import AggregateFunc
 
-max_ = AggregateFunc.NumericAggregateFunc('max')
-min_ = AggregateFunc.NumericAggregateFunc('min')
-count_ = AggregateFunc.NumericAggregateFunc('count')
-sum_ = AggregateFunc.NumericAggregateFunc('sum')
-avg_ = AggregateFunc.NumericAggregateFunc('avg')
--- query/operators.py
+++ query/operators.py
@@ -1,15 +1,20 @@
-from query.base import Operator
+from query.base import Operator, AggregateFunc
 
+# ------------------------------------
+#       Boolean Operators
+# ------------------------------------
 eq = Operator.BinaryBoolean('=')
 lt = Operator.BinaryBoolean('<')
 gt = Operator.BinaryBoolean('>')
 le = Operator.BinaryBoolean('<=')
 ge = Operator.BinaryBoolean('>=')
 
+# ------------------------------------
+#       Logical Boolean Operators
+# ------------------------------------
 neg = Operator.UnaryBoolean('!')
 and_ = Operator.BinaryBoolean('AND')
 or_ = Operator.BinaryBoolean('OR')
-
 
 # Note: != is just !( x = y ). Think about if we want to keep it.
 # ne = Operator.BinaryBoolean('!=')
@@ -19,3 +24,12 @@ or_ = Operator.BinaryBoolean('OR')
 # minus = Operator.BinaryNumeric('-')
 # multiply = Operator.BinaryNumeric('*')
 # divide = Operator.BinaryNumeric('/')
+
+# ------------------------------------
+#       Aggregate Operators
+# ------------------------------------
+max_ = AggregateFunc.NumericAggregateFunc('max')
+min_ = AggregateFunc.NumericAggregateFunc('min')
+count_ = AggregateFunc.NumericAggregateFunc('count')
+sum_ = AggregateFunc.NumericAggregateFunc('sum')
+avg_ = AggregateFunc.NumericAggregateFunc('avg')
