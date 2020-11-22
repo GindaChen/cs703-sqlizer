add2b4a7c27203e5a6852d8dc9b50bb01c579878
Shawn Zhong <github@shawnzhong.com>
2020-11-23 03:31:43 +0800

    w2v

--- query/confid.py
+++ query/confid.py
@@ -2,6 +2,10 @@
 # - Names of schema elements: Since our query sketches contain natural language hints for each hole, we can utilize table and column names in the database schema to assign confidence scores.
 # - Foreign and primary keys: Since foreign keys provide links between data in two different database tables, join operations that involve foreign keys have a higher chance of being the intended term.
 # - Database contents: Our approach also uses the contents of the database when assigning scores to queries. For instance, a candidate term sigma_phi(T) is relatively unlikely to occur in the target query if there are no entries in relation T satisfying predicate phi
+import os
+from pathlib import Path
+
+import gensim.downloader
 
 from query.base import Hint
 from query.expr import Entity, AbstractTable, AbstractColumns, Value, Column, Table, \
@@ -11,7 +15,7 @@ from database.table import DatabaseColumn
 class BaseConfid():
     def __init__(self):
         pass
-    
+
     @classmethod # compose a list of confidence
     def compose(confids):
         p = 1
@@ -21,7 +25,7 @@ class BaseConfid():
 
     def __mul__(self, other):
         return BaseConfid.compose([self, other])
-    
+
     # used by sort()
     def __lt__(self, other):
         return self.score < other.score
@@ -31,9 +35,9 @@ class BaseConfid():
 class HintConfid(BaseConfid):
     def __init__(self, hint: Hint, name: str):
         super().__init__()
-        self.score = 0 # to set
-        # TODO: Word2Vec...
-        pass
+        gensim.downloader.BASE_DIR = Path(__file__).parent.parent / "gensim-data"
+        glove_vectors = gensim.downloader.load('glove-twitter-25')
+        self.score = glove_vectors.similarity(name, hint)
 
 
 class JoinConfid(BaseConfid):
--- query/test_infer.py
+++ query/test_infer.py
@@ -1,15 +1,8 @@
+from database.table import Database, DatabaseTable
 
-from query.infer import SketchCompl, inferTable
-from query.expr import Entity, AbstractTable, AbstractColumns, Value, Column, \
-    Table, GroupAgg, Aggregation, Predicate, Projection, Selection, Join
-from query.confid import BaseConfid, HintConfid, JoinConfid, PredConfid
-from database.table import db, Database
 
-def test_inferTable():
-    old_db = db
+def test_infer_table():
     db = Database("test_inferTable_db")
-    db.add_column("test_col1")
-    # TODO: finish word2vec so this test could work
-
-    db = old_db # restore
-
+    table = DatabaseTable("test_table")
+    table.add_column("test_col1")
+    db.tables["test_table"] = table
