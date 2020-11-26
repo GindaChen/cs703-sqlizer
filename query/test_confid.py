3ca03cc95e609c2680438051a21826eee27ce5e5
Chenhao Ye <ye.chenhao.ych@gmail.com>
2020-11-26 16:03:36 -0600

    add test_CastConfid

--- query/test_confid.py
+++ query/test_confid.py
@@ -8,6 +8,7 @@ from query.type import boolean, numeric, string
 import query.operators as ops
 import query.params as params
 
+
 def getForeignDB():
     pushDatabase("db_test_foreign")
     db = getDatabase()
@@ -26,6 +27,20 @@ def getForeignDB():
     
     return db
 
+def getTinyDB():
+    pushDatabase("db_test_tiny")
+    db = getDatabase()
+
+    db.add_table("t1")
+
+    t1 = db["t1"]
+    t1.add_column("c1n", numeric)
+    t1.add_column("c1s", string)
+    t1.add_column("c1b", boolean)
+
+    return db
+
+
 def test_JoinConfid():
     getForeignDB()
 
@@ -39,5 +54,27 @@ def test_JoinConfid():
     assert len(res) == 6 * 6
     assert "SELECT t1.c1n\nFROM t1 JOIN t2 ON t1.c1s = t2.c2s" in res
     assert "SELECT t1.c1n\nFROM t1 JOIN t2 ON t1.c1s = t2.c2s_" in res[:12]
+
+    popDatabase()
+
+
+# similar to test_inferValue, but check confidence
+def test_CastConfid():
+    getTinyDB()
+
+    s = Selection(Table(hint=Hint("t1")),
+        Predicate(ops.ge, Column(hint=Hint("c1")), Value("2010")) )
+    ps = Projection(s, AbstractColumns(c=Column(hint=Hint("c1n"))))
+    sc_list = ps.getCandidates()
+    res = [ps.unparse(sketch_compl=sc) for sc in sc_list]
+
+    for r in res:
+        print(r)
+
+
+    assert len(res) == 3 * 3
+    assert "SELECT t1.c1n\nFROM t1\nWHERE (t1.c1n >= 2010)" in res
+    assert "SELECT t1.c1s\nFROM t1\nWHERE (t1.c1s >= \"2010\")" in res
+    assert "SELECT t1.c1n\nFROM t1\nWHERE (t1.c1s >= \"2010\")" == res[0]
 
     popDatabase()
