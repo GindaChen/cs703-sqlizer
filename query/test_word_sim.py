6676eda7aededad0181e88c11d09f9f1b88fceef
Shawn Zhong <github@shawnzhong.com>
2020-11-27 00:28:19 +0800

    Update test_word_sim.py

--- query/test_word_sim.py
+++ query/test_word_sim.py
@@ -1,14 +1,16 @@
-def test_word_sim():
-    from query.word_sim import ws_model
+from query.word_sim import ws_model
 
-    def print_ws(w1, w2):
-        print(w1, w2, ws_model.similarity(w1, w2))
 
+def print_ws(w1, w2):
+    print(w1, w2, ws_model.similarity(w1, w2))
+
+
+if __name__ == '__main__':
     print_ws("hi", "hello")
     print_ws("papers", "publication")
     print_ws("papers", "write")
     print_ws("papers", "author")
 
-
-if __name__ == '__main__':
-    test_word_sim()
+    while True:
+        w1, w2 = input("please type two words:\n").split()
+        print_ws(w1, w2)
