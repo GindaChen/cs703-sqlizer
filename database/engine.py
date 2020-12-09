610ff53e19dbaa6ae3c9d54185fb4f73db6d3324
GindaChen <32371474+GindaChen@users.noreply.github.com>
2020-12-09 15:00:08 -0600

    Add some type support

--- database/engine.py
+++ database/engine.py
@@ -12,17 +12,26 @@ def LoadDatabase(path, db_name=None):
     pushDatabase(db_name, conn)
     db = getDatabase()
     foreign_refs = [] # (primary table name, primary column name, foreign table name, foreign column name)
-    for tbl_name, in schema.fetchall():
+    table_names = [i[0] for i in schema.fetchall()]
+
+    # Filter some non-tables
+    filtered_list = ["sqlite_sequence"]
+    table_names = [i for i in table_names if i not in filtered_list]
+
+    for tbl_name in table_names:
         db.add_table(tbl_name)
         tbl = db[tbl_name]
-        for row in cur.execute(f'PRAGMA table_info("{tbl_name}")'):
+
+        col_info = list(cur.execute(f'PRAGMA table_info("{tbl_name}")'))
+        for row in col_info:
             col_name = row[1]
-            if row[2] == "INTEGER":
+            col_type_ = row[2].casefold()
+            if col_type_ in ["integer", "float"]:
                 col_type = numeric
-            elif row[2] == "TEXT":
+            elif col_type_ in ["text", "longtext"] or "varchar" in col_type_:
                 col_type = string
             else:
-                raise TypeError(f"Unsupported Type: {row[2]}")
+                raise TypeError(f"Unsupported Type for column {col_name}: {row[2]}")
             tbl.add_column(col_name, col_type)
         for row in cur.execute(f'PRAGMA foreign_key_list("{tbl_name}")'):
             foreign_refs.append((row[2], row[4], tbl_name, row[3]))
